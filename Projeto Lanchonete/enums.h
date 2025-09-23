022c45b5b7f822b0fe29c141fa2e2ad32dcebeb1
Arthur Melo Gusmão <100497176+GameL0@users.noreply.github.com>
2025-09-23 16:54:16 -0300

    Começo do Projeto 23.09

---
+++ Projeto Lanchonete/enums.h
@@ -0,0 +1,32 @@
+// Enum para o status de um item individual do pedido
+typedef enum{
+    AGUARDANDO_PREPARADO,
+    EM_PREPARO,
+    PRONTO
+} StatusItem;
+
+// Enum para o status geral de um pedido completo
+typedef enum{
+    RECEBIDO,
+    PREPARANDO_ITENS,
+    AGUARDANDO_MONTAGEM,
+    ENTREGUE,
+    ATRASADO
+} StatusPedido;
+
+// Enum para o status de um funcionário
+typedef enum{
+    LIVRE,
+    OCUPADO
+} StatusFuncionario;
+
+// Enum para as diferentes habilidades que um funcionário pode ter
+typedef enum{
+    HABILIDAE_CAIXA,
+    HABILIDADE_SEPARACAO,
+    HABILIDADE_SANDUICHE,
+    HABILIDADE_BATATA,
+    HABILIDADE_SUCO,
+    HABILIDADE_BEBIDA, // Refrigerantes e Milk-shakes
+    HABILIDADE_MONTAGEM
+} Habilidade;
---
+++ Projeto Lanchonete/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "enums.h"
+
+// --- Estruturas dos Pedidos ---
+
+// Representa um item individual (ex: um sanduiche, uma batata)
+typedef struct{
+    char nome[50];
+    int tempo_preparo_total;
+    int tempo_restante_preparo;
+    StatusItem status;
+} ItemPedido;
+
+// Representa um pedido completo de um cliente
+typedef struct{
+    int id;
+    ItemPedido itens[4] // Um pedido tem no máximo 4 itens (2 de comer, 2 de beber)
+    int num_itens;
+
+    int tempo_chegada; // Guarda o "tempo_global" em que o pedido chegou
+    int tempo_preparo_total; // Chave da prioridade para o Heap. Calculado na chegada.
+    StatusPedido status;
+} Pedido;
+
+// --- Estruturas dos Recursos da Lanchonete ---
+
+// Representa um funcionário e suas capacidades
+typedef struct{
+    int id;
+    Habilidade habilidades[3]; // Um funcionario pode ter multiplas habilidades
+    int num_habilidade;
+    StatusFuncionario status;
+    int id_pedido_atual; // Para saver qual pedido ele está trabalhando
+} Funcionario;
+
+// Representa um equipamento com capacidade limitada (chapa, fritadeira, etc)
+typedef struct{
+    char nome[50].
+    int capacidade_maxima; // Sera definido pelas regras do nome Arthur
+    int capacidade_usada;
+} Equipamento;
+
+// --- Estrutura de Controle (Fila de Prioridades) ---
+
+typedef struct
+{
+    Pedido *pedidos; // Vetor dinâmico para armazenar os pedidos
+    int quantidade; // Quantidade atual de pedidos na fila
+    int capacidade; // Tamanho máximo do vetor
+} Heap;
+
+
+
+int main(){
+    return 0;
+}
